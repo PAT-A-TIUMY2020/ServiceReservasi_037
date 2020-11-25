7da95d73ece7d96cd49eeca33c53c3065883836c
Muhammad Hirdhan Aghnia <mhirdhanaghnia@gmail.com>
2020-11-25 19:56:22 +0700

    Done

---
+++ ServiceReservasi/ServiceReservasi/IService1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceReservasi
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        //method proses input data
+        string pemesanan(string IdPemesanan, string NamaCustomer, string NoTelp, int JumlahPemesanan, string IdLokasi);
+        [OperationContract]
+        string editPemesanan(string idPemesanan, string namaCustomer);
+        [OperationContract]
+        string deletePemesanan(string idPemesanan);
+        [OperationContract]
+        //Menampilkan data yang ada di database
+        List<CekLokasi> reviewLokasi();
+        [OperationContract]
+        //Menampilkan detail lokasi
+        List<DetailLokasi> DetailLokasi();
+        [OperationContract]
+        List<Pemesanan> Pemesanan();
+    }
+
+    //Daftar Lokasi
+    [DataContract]
+    public class CekLokasi
+    {
+        [DataMember]
+        public string IDLokasi { get; set; }
+        [DataMember]
+        public string NamaLokasi { get; set; }
+        [DataMember]
+        public string Deskripsi { get; set; }
+    }
+
+    //Menampilkan detail lokasi
+    [DataContract]
+    public class DetailLokasi
+    {
+        [DataMember]
+        public string IdLokasi { get; set; }
+        [DataMember]
+        public string NamaLokasi { get; set; }
+        [DataMember]
+        public string DeskripsiFull { get; set; }
+        [DataMember]
+        public int Kuota { get; set; }
+    }
+
+    [DataContract]
+    public class Pemesanan
+    {
+        [DataMember]
+        public string IdPemesanan { get; set; }
+        [DataMember]
+        public string NamaCustomer { get; set; }
+        [DataMember]
+        public string NoTelp { get; set; }
+        [DataMember]
+        public int JumlahPemesanan { get; set; }
+        [DataMember]
+        public string IdLokasi { get; set; }
+    }
+}
---
+++ ServiceReservasi/ServiceReservasi/Service1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace ServiceReservasi
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+
+        string connectionString = "Data Source=LAPTOP-5QOEH7MN;Initial Catalog=WCFReservasi;Persist Security Info=True;User ID=sa;Password=iDhanzaghnia99";
+        SqlConnection connection;
+        //untuk menghubungkan database
+        SqlCommand com;
+
+
+        public string deletePemesanan(string idPemesanan)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<DetailLokasi> DetailLokasi()
+        {
+            //deklarasi nama List
+            List<DetailLokasi> LokasiFull = new List<DetailLokasi>();
+            try
+            {
+                string sql = "select ID_Lokasi, Nama_Lokasi, Deskripsi_Full, Kuota from dbo.Lokasi";
+                connection = new SqlConnection(connectionString);
+                com = new SqlCommand(sql, connection);
+                connection.Open();
+                SqlDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    DetailLokasi data = new DetailLokasi();
+                    //array
+                    data.IdLokasi = reader.GetString(0);
+                    data.NamaLokasi = reader.GetString(1);
+                    data.DeskripsiFull = reader.GetString(2);
+                    data.Kuota = reader.GetInt32(3);
+                    LokasiFull.Add(data);
+                }
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return LokasiFull;
+        }
+
+        public string editPemesanan(string idPemesanan, string namaCustomer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string pemesanan(string IdPemesanan, string NamaCustomer, string NoTelp, int JumlahPemesanan, string IdLokasi)
+        {
+            string a = "gagal";
+            try
+            {
+                string sql = "insert into dbo.Pemesanan values ('" + IdPemesanan + "','" + NamaCustomer + "','" + NoTelp + "'," + JumlahPemesanan + ",'" + IdLokasi + "')";
+
+                connection = new SqlConnection(connectionString);
+                com = new SqlCommand(sql, connection);
+                connection.Open();
+                com.ExecuteNonQuery();
+                connection.Close();
+                a = "Sukses";
+            }
+            catch (Exception es)
+            {
+                Console.WriteLine(es);
+            }
+            
+            return a;
+        }
+
+        public List<Pemesanan> Pemesanan()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CekLokasi> reviewLokasi()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
